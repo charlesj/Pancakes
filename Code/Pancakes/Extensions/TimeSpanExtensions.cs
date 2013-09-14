5b1e18fdf344befe0c33f778ec7f05e24623fa30
Josh Charles <josh.charles@gmail.com>
2013-09-14 12:49:46 -0700

    Added Basic Mapping / Validation wrappers.
    These need to be cleaned up and tested.

---
+++ Code/Pancakes/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,88 @@
+﻿namespace Pancakes.Extensions
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// The time span extensions.
+    /// </summary>
+    public static class TimeSpanExtensions
+    {
+        /// <summary>
+        /// The in milliseconds.
+        /// </summary>
+        /// <param name="ts">
+        /// The ts.
+        /// </param>
+        /// <returns>
+        /// The System.Int32.
+        /// </returns>
+        public static int InMilliseconds(this TimeSpan ts)
+        {
+            return (int)ts.TotalMilliseconds;
+        }
+
+        /// <summary>
+        /// The ago.
+        /// </summary>
+        /// <param name="ts">
+        /// The ts.
+        /// </param>
+        /// <returns>
+        /// The System.DateTime.
+        /// </returns>
+        public static DateTime Ago(this TimeSpan ts)
+        {
+            return DateTime.Now - ts;
+        }
+
+        /// <summary>
+        /// The from now.
+        /// </summary>
+        /// <param name="ts">
+        /// The ts.
+        /// </param>
+        /// <returns>
+        /// The System.DateTime.
+        /// </returns>
+        public static DateTime FromNow(this TimeSpan ts)
+        {
+            return DateTime.Now + ts;
+        }
+
+        /// <summary>
+        /// Generates a string following this format X days x hours x minutes x seconds, only including the components that are not zero.
+        /// </summary>
+        /// <param name="ts">
+        /// The timespan to use
+        /// </param>
+        /// <returns>
+        /// The System.String.
+        /// </returns>
+        public static string ToWords(this TimeSpan ts)
+        {
+            var sb = new StringBuilder();
+            if (ts.Days != 0)
+            {
+                sb.Append(string.Format("{0} {1} ", ts.Days, ts.Days.Pluralize("day", "days")));
+            }
+
+            if (ts.Hours != 0)
+            {
+                sb.Append(string.Format("{0} {1} ", ts.Hours, ts.Hours.Pluralize("hour", "hours")));
+            }
+
+            if (ts.Minutes != 0)
+            {
+                sb.Append(string.Format("{0} {1} ", ts.Minutes, ts.Minutes.Pluralize("minute", "minutes")));
+            }
+
+            if (ts.Seconds != 0)
+            {
+                sb.Append(string.Format("{0} {1} ", ts.Seconds, ts.Seconds.Pluralize("second", "seconds")));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Code/Pancakes/Validation/ValidationException.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ValidationException.cs" company="Josh Charles">
+//   Copyright (c) 2013 Josh Charles.  Released under the MIT license.
+// </copyright>
+// <summary>
+//   The validation exception.  This exception is meant to be thrown when an object is encountered
+//   that should be valid at that point, but is not.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Pancakes.Validation
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The validation exception.  This exception is meant to be thrown when an object is encountered
+    /// that should be valid at that point, but is not.
+    /// </summary>
+    public class ValidationException : Exception
+    {
+        /// <summary>
+        /// The data.
+        /// </summary>
+        private readonly System.Collections.IDictionary data;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidationException"/> class.
+        /// </summary>
+        public ValidationException() : this(new Result { IsValid = false })
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidationException"/> class.
+        /// </summary>
+        /// <param name="result">
+        /// The result.
+        /// </param>
+        public ValidationException(Result result) : base("There was a problem validating the submission. See data property for messages.")
+        {
+            this.data = new Dictionary<string, string>();
+            foreach (var message in result.FailureMessages)
+            {
+                this.data.Add(message.PropertyName, message.Message);
+            }
+        }
+
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        public override System.Collections.IDictionary Data
+        {
+            get
+            {
+                return this.data;
+            }
+        }
+    }
+}
