39c26db3cb917c0ec8f88de2a522d2a3f0923ec1
Josh Charles <josh.charles@gmail.com>
2013-09-15 11:58:19 -0700

    Added Tests for mapping and validation

---
+++ Code/Pancakes.Tests/MappingTests/MappingServiceTests.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MappingServiceTests.cs" company="Josh Charles">
+//   Copyright (c) 2013 Josh Charles.  Released under the MIT license.
+// </copyright>
+// <summary>
+//   Defines the MappingServiceTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Pancakes.Tests.MappingTests
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using AutoMapper;
+
+    using Pancakes.Mapping;
+
+    using Xunit;
+
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Testing Class. No Documentation Required")]
+    public class MappingServiceTests
+    {
+        [Fact]
+        public void CanMapFromFootballToSoccer()
+        {
+            var config = new SportMappingConfiguration();
+            config.Configure();
+
+            var mapper = new AutoMapperMappingService();
+
+            var soccer = new SoccerPlayer { Name = "Fozzy" };
+
+            var mapped = mapper.Map<SoccerPlayer, FootballPlayer>(soccer);
+
+            Assert.Equal("Fozzy", mapped.Name);
+        }
+
+        private class FootballPlayer
+        {
+            public string Name { get; set; }
+        }
+
+        private class SoccerPlayer
+        {
+            public string Name { get; set; }
+        }
+
+        private class SportMappingConfiguration : IMappingConfiguration
+        {
+            public void Configure()
+            {
+                Mapper.CreateMap<FootballPlayer, SoccerPlayer>();
+                Mapper.CreateMap<SoccerPlayer, FootballPlayer>();
+            }
+        }
+    }
+}
