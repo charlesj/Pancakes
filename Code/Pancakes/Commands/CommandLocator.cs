4c7350f04fcc699f7fb84a2fd31305a977eb4c79
Josh Charles <josh.charles@gmail.com>
2014-05-30 08:12:34 -0700

    Added First test for command locator.  Added CI status to readme

--- Code/Pancakes/Commands/CommandLocator.cs
+++ Code/Pancakes/Commands/CommandLocator.cs
@@ -1,10 +1,30 @@
 ﻿namespace Pancakes.Commands
 {
+    using System;
+
+    using Pancakes.ServiceLocater;
+
     /// <summary>
     /// The command locator.
     /// </summary>
     public class CommandLocator : ICommandLocator
     {
+        /// <summary>
+        /// The service locater.
+        /// </summary>
+        private readonly IServiceLocater serviceLocater;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandLocator"/> class.
+        /// </summary>
+        /// <param name="serviceLocater">
+        /// The service locater.
+        /// </param>
+        public CommandLocator(IServiceLocater serviceLocater)
+        {
+            this.serviceLocater = serviceLocater;
+        }
+
         /// <summary>
         /// The locate command.
         /// </summary>
@@ -19,7 +39,14 @@
         /// </returns>
         public ICommand<TRequest, TResponse> LocateCommand<TRequest, TResponse>()
         {
-            throw new CommandNotFoundException(new[] { typeof(TResponse), typeof(TRequest) });
+            try
+            {
+                return this.serviceLocater.GetService<ICommand<TRequest, TResponse>>();
+            }
+            catch (Exception)
+            {
+                throw new CommandNotFoundException(new[] { typeof(TResponse), typeof(TRequest) });
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 #Pancakes
 
+[![Build status](https://ci.appveyor.com/api/projects/status/c5gmf2rsbxmvdrio)](https://ci.appveyor.com/project/charlesj/pancakes)
 
 Pancakes brings together well known .NET libraries into a cogent stack that can be used immediately.  Things like logging, validation, and object mapping are wrapped into interfaces that can be injected into their relevant classes.
 
